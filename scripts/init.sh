328142dd334192c01cf6950122043b0af21117a4
Ubuntu <ubuntu@ip-172-31-45-92.eu-west-1.compute.internal>
2026-02-04 15:24:19 +0000

    Update gitignore and init.sh to not archive production model when starting API.

--- scripts/init.sh
+++ scripts/init.sh
@@ -7,17 +7,17 @@ if [ -f .env ]; then
 fi
 
 # Clean up automated splits data
-rm -rf data/automated_splits/split_*
-rm -f data/automated_splits/metadata.yaml
+#rm -rf data/automated_splits/split_*
+#rm -f data/automated_splits/metadata.yaml
 
-echo "Archive all models on MLflow..."
-python scripts/archive_all_models.py
+#echo "Archive all models on MLflow..."
+#python scripts/archive_all_models.py
 
 echo "Generate all preprocessing outputs..."
 python src/data/preprocess.py
 
-echo "Generate manually training splits..."
-python src/data/training_data_splits_by_year.py
+#echo "Generate manually training splits..."
+#python src/data/training_data_splits_by_year.py
 
 # Start the application
 echo "Starting API..."
